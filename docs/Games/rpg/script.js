280526beccdfd0ebf76250240cdf9335e007d8a8
Rafael Menezes <faelgmp@gmail.com>
2022-10-13 22:20:15 -0300

    inteligencia em 50% projeto rpg

--- docs/Games/rpg/script.js
+++ docs/Games/rpg/script.js
@@ -1,4 +1,9 @@
 var tela = document.getElementById('tela')
+var criar_inimigo = document.createElement("div")
+criar_inimigo.className = 'inimigo1'
+tela.appendChild(criar_inimigo)
+
+setInterval(movimento_inimigo1, 100)
 
 var player_parametros = {
     vida: 0,
@@ -10,18 +15,20 @@ var player = document.getElementById('player');
 var andar_x = 50;
 var andar_y = 50;
 
+var inimigo1 = document.querySelector('div.inimigo1')
+var inimigo1_x = 50
+var inimigo1_y = 10
+
 function movimento(event){
     var andar = event.code;
 
     if(andar == 'KeyA' && andar_y > 1){
         andar_y += -1
         player.style.left = `${andar_y}%`;
-        movimento_inimigo1()
 
     }else if(andar == 'KeyD' && andar_y < 99){
         andar_y += 1
         player.style.left = `${andar_y}%`;
-        criar_inimigo1()
 
     }else if(andar == 'KeyW' && andar_x > 2){
         andar_x += -1
@@ -40,34 +47,36 @@ function morte(){
     }
 }
 
-function criar_inimigo1(){
-    let criar_inimigo = document.createElement("div")
-    criar_inimigo.className = 'inimigo1'
-    tela.appendChild(criar_inimigo)
-}
-
 function movimento_inimigo1(){
-    let ale = Math.floor(Math.random()*99)
 
-    let inimigo1 = document.querySelector('div.inimigo1')
-    let inimigo1_x = 50
-    let inimigo1_y = 50
+    var dados = document.getElementById('dados')
+
+    inimigo1.style.left = `${inimigo1_y}%`;
+    inimigo1.style.top =`${inimigo1_x}%`
     
-    if(ale <= 25 && inimigo1_y > 1){
+    if(Math.abs(andar_x - inimigo1_x) <= 4 && Math.abs(andar_y - inimigo1_y) <= 2){
+        morte()
+
+    }else if(andar_y < inimigo1_y && inimigo1_y > 1){
         inimigo1_y += -1
         inimigo1.style.left = `${inimigo1_y}%`;
 
-    }else if(ale <= 50 && inimigo1_y < 99){
+    }else if(andar_y > inimigo1_y && inimigo1_y < 99){
         inimigo1_y += 1
         inimigo1.style.left = `${inimigo1_y}%`;
 
-    }else if(ale <= 75 && inimigo1_x > 2){
+    }else if(andar_x < inimigo1_x && inimigo1_x > 2){
         inimigo1_x += -1
         inimigo1.style.top =`${inimigo1_x}%`
 
-    }else if(ale <= 100 && inimigo1_x < 98){
+    }else if(andar_x > inimigo1_x && inimigo1_x < 98){
         inimigo1_x += 1
         inimigo1.style.top =`${inimigo1_x}%`
 
+    }else{
+        morte()
     };
+
+    dados.innerHTML= `
+    Distancia Y ${Math.abs(andar_y - inimigo1_y)}, X ${Math.abs(andar_x - inimigo1_x)}. andar Y ${andar_y}, X ${andar_x}. inimigo1 Y ${inimigo1_y}, X ${inimigo1_x}.`
 }
